33a4b6ab337c2102329e1d4fe9fbea905aa3b235
hay-kot <hay-kot@pm.me>
2020-09-11 17:03:57 -0800

    Updated Logger

--- cookiecutter-python/{{cookiecutter.project_slug}}/{{ cookiecutter.project_slug }}/custom_logger.py
+++ cookiecutter-python/{{cookiecutter.project_slug}}/{{ cookiecutter.project_slug }}/custom_logger.py
@@ -1,12 +1,22 @@
 import logging
+from pathlib import Path
 
 import coloredlogs
 
-logging.basicConfig(level=logging.DEBUG, )
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-coloredlogs.install(level='DEBUG', fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    filename="./{{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}.log",
+)
+coloredlogs.install(
+    level="DEBUG",
+    fmt="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
 
 logger = logging.getLogger(__name__)
+
 
 """ Logging Cheat Sheet
 logger.debug("this is a debugging message")
